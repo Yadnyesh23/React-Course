a76d0097f1f914b0d585b83c2923f5cc34ff8dfe
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2025-12-31 12:20:11 +0530

    Created Car using tailind and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,12 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Card'
 
 function App() {
 
   return (
     <>
       <h1 className='bg-grey-500 text-shadow-white p-4 rounded-2xl' >Tailwind test</h1>
+      <Card username="Yadnyesh" btnText="Follow"/>
+      <Card username="Stavan" btnText="Follow"/>
     </>
   )
 }
