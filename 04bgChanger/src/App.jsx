442ff5223eaf810a57abd227f1cab56671372bca
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-01 12:01:56 +0530

    Initialized Password Generator Project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
 
-
-
 function App() {
   const [color, setColor] = useState("olive")
 
   return (
-    <>
-     <div className='w-full h-screen ' style={{backgroundColor : color}}></div>
-    </>
+    <div
+      className="w-full h-screen transition-colors duration-300"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-white p-4 rounded-xl flex gap-3 shadow-lg">
+        <button
+          className="rounded-lg bg-red-500 px-4 py-2 text-white"
+          onClick={() => setColor('red')}
+        >
+          Red
+        </button>
+
+        <button
+          className="rounded-lg bg-green-500 px-4 py-2 text-white"
+          onClick={() => setColor('green')}
+        >
+          Green
+        </button>
+
+        <button
+          className="rounded-lg bg-blue-500 px-4 py-2 text-white"
+          onClick={() => setColor('blue')}
+        >
+          Blue
+        </button>
+      </div>
+    </div>
   )
 }
 
---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl text-white'>Password generator</h1>
+    </>
+  )
+}
+
+export default App
