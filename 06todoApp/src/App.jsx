8bea54f5ac7f112e7e31fc571bb062596871e81d
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-02 12:23:17 +0530

    Initialized Todo app

---
+++ 06todoApp/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-black w-full h-screen">
+      <h1 className="text-4xl text-white text-center">Todo App</h1>
+    </div>
+  )
+}
+
+export default App
