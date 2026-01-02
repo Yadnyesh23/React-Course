ff30d9b032a468b87c9502dc5dd032aea23833b7
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-02 16:08:27 +0530

    Initialized Expense tracker project

---
+++ 07ExpenseTracker/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="w-full h-screen bg-black text-white text-center">
+      <h1 className="text-4xl">Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default App
